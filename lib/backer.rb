0144d86da0863f3cd734cfb0c5e60023d930986b
Ricky Rojas <rickyrojas19@gmail.com>
2019-05-21 21:39:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,10 +2,10 @@ class Backer
   attr_reader :backed_projects
   attr_accessor :name
   
-  @backed_projects = []
   
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
   
 end
